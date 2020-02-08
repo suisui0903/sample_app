2ae75f62080fef43f3babba17dc677ab04c198c4
川上鈴乃 <kawakamisuzuno@kawakamisuzunonoMacBook-Pro.local>
2020-02-08 19:12:11 +0900

    [finish]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -10,8 +10,14 @@ class TodolistsController < ApplicationController
 
         list.save
 
-        redirect_to '/top'
+        redirect_to todolist_path(list.id)
     end
+    def index
+        @lists = List.all
+      end
+      def show
+        @list = List.find(params[:id]) 
+      end
     private
 
     def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
 
   post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
+
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
+      # .../todolists/1 や .../todolists/3 に該当する
 end
