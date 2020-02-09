34c2e132d1e24be0d69b2fc7998c85084c8fb2a8
川上鈴乃 <kawakamisuzuno@kawakamisuzunonoMacBook-Pro.local>
2020-02-09 16:13:06 +0900

    [finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
       # .../todolists/1 や .../todolists/3 に該当する
+      get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
 end
